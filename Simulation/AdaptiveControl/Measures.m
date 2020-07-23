b6d05e37973c9ad5b13db536e6d4b9071f1738b5
Bespi123 <62658381+Bespi123@users.noreply.github.com>
2020-07-22 21:11:07 -0500

    VSCCT and EULERINT measures
    Mediciones para comparar la efectividad de las leyes de control

--- Simulation/AdaptiveControl/Measures.m
+++ Simulation/AdaptiveControl/Measures.m
@@ -1,15 +1,12 @@
 function [Out] = Measures (entr)
 %Definiendo entradas
  q=[entr(1),entr(2),entr(3),entr(4)]';
- w=[entr(5),entr(6),entr(7)]';
- J=[entr(8),entr(11),entr(12);
-    entr(11),entr(9),entr(13);
-    entr(12),entr(13),entr(10)];     %Tensor de inercia calculado
-%Ecuacion de energia cinetica    
-    K=w'*J*w;
+ u=[entr(5),entr(6),entr(7)]';
+%Modulo de torque al cuadrado
+umodpow2=entr(5)^2+entr(6)^2+entr(7)^2;
 %Ecuación de angulo euler
     Euler_ang=2*acos(q(1));
 %Ecuacion de salida
-    Out=[K,Euler_ang]';
+    Out=[umodpow2,Euler_ang]';
 end
 
