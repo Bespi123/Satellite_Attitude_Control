4bdb87823a31cdf5453badeba7143811bd201563
Bespi123 <62658381+Bespi123@users.noreply.github.com>
2020-10-08 08:42:55 -0500

    Update rwTest.ino

--- Playground/rwTest/rwTest.ino
+++ Playground/rwTest/rwTest.ino
@@ -4,12 +4,12 @@
 #define Sensor_IM3  26
 
 //Reaction Wheels PWM Pins
-#define pin_PWM_M1  40
+#define pin_PWM_M1  3
 #define pin_PWM_M2  39
 #define pin_PWM_M3  38
 
 //Reaction Wheels direction pines
-#define pin_dir_M1  13
+#define pin_dir_M1  2
 #define pin_dir_M2  12
 #define pin_dir_M3  11
 
@@ -31,28 +31,28 @@ pinMode(pin_dir_M1,OUTPUT);
 void loop() {
   Serial.println("CW");
   digitalWrite(pin_dir_M1,HIGH);
-//  for(int i=255;i>0;i=i-50){
-//    Serial.println(i);
-//    analogWrite(pin_PWM_M1,i);
-//    delay(3000);
-//  }
-//  for(int i=0;i<255;i=i+50){
-//    Serial.println(i);
-//    analogWrite(pin_PWM_M1,i);
-//    delay(3000);
-//  }
+  for(int i=255;i>0;i=i-50){
+    Serial.println(i);
+    analogWrite(pin_PWM_M1,i);
+    delay(3000);
+  }
+  for(int i=0;i<255;i=i+50){
+    Serial.println(i);
+    analogWrite(pin_PWM_M1,i);
+    delay(3000);
+  }
   delay(1000);
   Serial.println("CCW");
   digitalWrite(pin_dir_M1,LOW);
-//  for(int i=255;i>0;i=i-50){
-//    Serial.println(i);
-//    analogWrite(pin_PWM_M1,i);
-//    delay(3000);
-//  }
-//  for(int i=0;i<255;i=i+50){
-//    Serial.println(i);
-//    analogWrite(pin_PWM_M1,i);
-//    delay(3000);
-//  }
+  for(int i=255;i>0;i=i-50){
+    Serial.println(i);
+    analogWrite(pin_PWM_M1,i);
+    delay(3000);
+  }
+  for(int i=0;i<255;i=i+50){
+    Serial.println(i);
+    analogWrite(pin_PWM_M1,i);
+    delay(3000);
+  }
 delay(1000);
 }
