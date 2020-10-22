c512aa63e304518dae6e39a95ee348b43d87ffee
Bespi123 <b_antonioesp@hotmail.com>
2020-10-22 16:24:06 -0500

    Create Parameterident.m

---
+++ Simulation/Scripts/Brushless model/Parameterident.m
@@ -0,0 +1,11 @@
+%**************************************************************************
+% AUTHOR: Brayan Espinoza 22/10/2020
+% DESCRIPTION: 
+% This program perform the parameter estimation for a brushless motor.
+% IMPORTANT: This program is based by the idnlgreydemol developed by 
+% Mathworks. The comand to open this example and see in more detail is 
+% shown:
+%       openExample('ident/idnlgreydemo1')
+% 
+% *************************************************************************
+
