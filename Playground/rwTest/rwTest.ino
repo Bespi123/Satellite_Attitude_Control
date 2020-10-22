0bc3e66d2e8be71f4022e1abc62c43b720a9c8b5
Bespi123 <b_antonioesp@hotmail.com>
2020-10-22 13:08:36 -0500

    Update rwTest.ino

--- Playground/rwTest/rwTest.ino
+++ Playground/rwTest/rwTest.ino
@@ -1,45 +1,91 @@
+/************************************************************************************
+ * PROGRAM NAME: Reaction wheels prove and caraxterization
+ * DESCRIPTION: 
+ * This program test the CubeSat reaction wheels with an trapezoidal wave form as input
+ * the program measure reaction wheels angular rates, and total current aplied to it. The
+ * data in measure and sending to the PC serial port each 200ms.
+ * 
+ * IMPORTANT: The program is based in the program developed by Ing. Hammerly 
+ * Mamani Valencia 
+ * 
+ * PROGRAM NAME: Reaction wheel test and caracterization 
+ * 
+ * AUTHOR: Bach. Brayan Espinoza (Bespi123)
+ * 
+ * DEPENDENCIES:
+ * - Wire
+ * - stdint
+ * - stdbool 
+ * - inc/hw_ints.h, and others than can be obtained in:
+ *   https://github.com/energia/Energia/tree/master/hardware/cc2600emt/cores/cc2600emt
+ */
+
+/***************************************************************************************
+ **                              Libraries and dependencies                           ** 
+ ***************************************************************************************
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+
+/***************************************************************************************
+ **                              Program definitions                                  ** 
+ ***************************************************************************************
+ */
 //Current sensor Pins
-#define Sensor_IM1  A0
+#define Sensor_IM1  28
 //Reaction Wheels PWM Pins
-#define pin_PWM_M1  4
+#define pin_PWM_M1  40
 //Reaction Wheels direction pines
-#define pin_dir_M1  3
+#define pin_dir_M1  32
 //Reaction Wheel ecoder Pines
-#define pin_enc_M1  2
+#define pin_enc_M1  36
 //Debug flag
 #define DEBUG true
 
-#include <SD.h>
-#include <SPI.h>
-
-//Define SD card pin
-const int chipSelect = 53;
-
+/***************************************************************************************
+ **                              Program variables                                    ** 
+ ***************************************************************************************
+ */
 //Trapezoidal wave form constants
 bool UpDown = false;
 int i=0;
 int n=0;
 
+//Timer interruption Flag 
+bool sta_procs=0;                     
+
 //Encoder variables
 volatile int cuentas1=0;
-
-//Reaction wheek angular rate
-float Wrw;
+volatile int vel_M1;
 
 //Read current constants
-int I_M1;
-const float  sensibi_ACS712=0.185;              // Sensibilidad de de ACS712 [V/A]
+volatile int16_t I_M1;
+const float  sensibi_ACS712=0.185;  //ACS712 [V/A]
 
-
-//Perform amount of time
-unsigned long time1;
-unsigned long dt;
-
+/***************************************************************************************
+ **                      Encoder external interruption                                ** 
+ ***************************************************************************************
+ */
 void ReadEnc(){
 if ((digitalRead(pin_enc_M1)))
        cuentas1++;                 //Increase counter  
 }
 
+
+/***************************************************************************************
+ **                          Get current Function                                     ** 
+ ***************************************************************************************
+ */
 int getSensorASC712(byte pin_sensor, byte samplesNumber)
 {
    long corrienteSum = 0;
@@ -57,31 +103,39 @@ int getSensorASC712(byte pin_sensor, byte samplesNumber)
   return (corriente); 
 }
 
+/***************************************************************************************
+ **                                  MAIN PROGRAM                                     ** 
+ ***************************************************************************************
+ */
 void setup() { 
-// Define inputs and outputs
+//Initialize serial port
 Serial.begin(9600);
-Serial.print("Initializing SD card...");
-
-// See if the card is present and can be initialized:
-if (!SD.begin(chipSelect)) {
-Serial.println("Card failed, or not present");
-}
-Serial.println("card initialized.");
 
 // Initialize Inputs and outPuts
 pinMode(pin_PWM_M1,OUTPUT);
 pinMode(pin_dir_M1,OUTPUT);
 pinMode(Sensor_IM1,INPUT);
 pinMode(pin_enc_M1,INPUT);
+
+//Attach external interrupt for the encoder
 attachInterrupt(digitalPinToInterrupt(pin_enc_M1), ReadEnc, RISING);
 
+// Set Timer 0
+MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);                    //Enable Timer 0 
+MAP_TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);                 //Set TIMER 0, in periodic mode
+MAP_TimerLoadSet(TIMER0_BASE, TIMER_A, MAP_SysCtlClockGet()/5);      //Set TIMER 0,overflow period: 200 mseg
+TimerIntRegister(TIMER0_BASE, TIMER_A, &Timer0IntHandler);           //Set Interruption function
+MAP_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);                 //Enable timer 0 with overflow
+
+//Set TIMER 0 interruption: counting start
+MAP_TimerEnable(TIMER0_BASE, TIMER_A); 
 }
 
 void loop() {
-//Get the amount of time
-time1=millis(); 
-
-// Performing trapezoidal wave form
+  
+if(sta_procs==1){
+  
+  // Performing trapezoidal wave form
   if(i<255 && UpDown == false){
     i++;
   } else if (i==255 && UpDown == false){
@@ -101,49 +155,40 @@ time1=millis();
       UpDown=false;
     }
   }
-//Send PWM value
-analogWrite(pin_PWM_M1, i);
 
-//Read current measure
- // I_M1=getSensorASC712(Sensor_IM1,20);
-//if((analogRead(Sensor_IM1))>3105){
- I_M1=analogRead(Sensor_IM1);
-//}
+  //Send PWM value
+  analogWrite(pin_PWM_M1, i);
 
-// Read angular velocity (RPM)
-//Wrw=cuentas1*60/(6*dt*0.0001);
-Wrw=cuentas1;
+  #if DEBUG
+    Serial.print(255-i);
+    Serial.print(",");
+    Serial.print(vel_M1);
+    Serial.print(",");
+    Serial.println(I_M1);
+  #endif
+
+  //Clean Interruption flag
+  sta_procs=0; 
+}
+}
+
+/***************************************************************************************
+ **                              Timer0 Interruption                                  ** 
+ ***************************************************************************************
+ */
+void Timer0IntHandler() {
+//Clean Overflow flag
+MAP_TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);       
+
+// Measurements of current
+I_M1=getSensorASC712(Sensor_IM1,20);
+   
+// Read encoders
+vel_M1=cuentas1;
+
+// Clear encoder varaibles
 cuentas1=0;
 
-delay(100);
-
-//Get dt
-dt=millis()-time1;
-time1=0;
-
-#if DEBUG
-  Serial.print(i);
-  Serial.print(",");
-  Serial.print(Wrw);
-  Serial.print(",");
-  Serial.print(I_M1);
-  Serial.print(",");
-  Serial.println(dt);
-#endif
-
-// open the file. note that only one file can be open at a time,
-// so you have to close this one before opening another.
-File dataFile = SD.open("RW3prueba1.txt", FILE_WRITE);
-
-// if the file is available, write to it:
-if (dataFile) {
-  dataFile.print(i);
-  dataFile.print(",");
-  dataFile.print(Wrw);
-  dataFile.print(",");
-  dataFile.print(I_M1);
-  dataFile.print(",");
-  dataFile.println(dt);
-  dataFile.close();
-}
+//Turn-on Interruption flag
+sta_procs=1;
 }
