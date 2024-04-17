088cd7e8f3b6678af566c9b813cb2150068d5c8c
Bespi123 <b_antonioesp@hotmail.com>
2024-04-16 19:47:55 -0500

    Add a sintonization function

--- myPaper/motorModel/BrushelessModel.m
+++ myPaper/motorModel/BrushelessModel.m
@@ -1,37 +1,39 @@
-%**************************************************************************
-% AUTHOR: Brayan Espinoza 3/10/2020
-% DESCRIPTION: 
-% This program contains the steady-state model for a brushless motor
-% developed by José A. Becerra-Vargas in the article "Estimation parameters 
-% and black box model of a brushless DC motor".
-% IMPORTANT: 
-%
-% *************************************************************************
+%function [dx,y] = BrushelessModel(t,x,u,kt,J,B,Kc,L,R,Ke,d,ws,varargin)
+function [dx,y] = BrushelessModel(t,x,u,kt,J,B,L,R,Ke,varargin)
+    %Define inputs
+    %u:     Vpwm (Vmean)
+    %kt:    Torque constant (N*m/A)
+    %J:     Rotor inertia (kg*m^2)
+    %B:     Constante de friccion viscosa (N*m*s)
+    %kc:    Constante de friccion de Coulomb (N*m*s)
+    %L:     Inductancia (H)
+    %R:     Resistencia (R)
+    %ke:    Constante contraelectromotriz (V/(Rad/s))
+    %d:     Starting Torque (N*m)   
+    %ws:    Stribeck angular rate (rad/s)
+    
+    %kt=25.5E-3;
+    %L=0.56E-3;
+    %R=1.2;
+    %Ke=0.85;
+    %d =0;
+    %ws = 1;
 
-function [x_dot,y] = BrushelessModel(t,x,u,kt,J,B,Kc,L,R,Ke,Tcte,varargin)
-%Define inputs
-%u:     Vpwm (Vmean)
-%kt:    Torque constant (N*m/A)
-%ke:    Constante contraelectromotriz (V/(Rad/s))
-%kc:    Constante de friccion de Coulomb (N*m*s)
-%J:     Rotor inertia (kg*m^2)
-%B:     Constante de friccion viscosa (N*m*s)
-%L:     Inductancia (H)
-%R:     Resistencia (R)
-%Tcte:  Torque de la carga
+    %%% State variables
+    w  = x(1);   %Reaction wheel angular rate
+    i  = x(2);   %Reaction wheel current face
 
-w=x(1);   %Reaction wheel angular rate
-i=x(2);   %Reaction wheel current face
-
-%Perform ecuations
-w_dot=1/J*(kt*i-B*w-Kc*sign(w)-Tcte);
-i_dot=1/L*(u-R*i-Ke*w);
-
-%x_dot vector
-x_dot=[w_dot,i_dot]';
-
-%Output equation (Angular rate)
-y=[x(1);x(2)];
-
+    %Perform ecuations
+    %w_dot=1/J*(kt*i-B*w-sign(w)*(Kc+d*exp(w^2/ws)));
+    
+    w_dot=1/J*(kt*i-B*w);
+    i_dot=1/L*(u-R*i-Ke/J*w);
+    
+    %x_dot vector
+    dx=[w_dot,i_dot]';
+    
+    %Output equation (Angular rate)
+    y=[x(1);
+        x(2)
+        ];
 end
-
